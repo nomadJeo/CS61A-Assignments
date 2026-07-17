40142ffac05c15987bb51f18c93969abfa844fab
221250153-Wang Jing <221250153@smail.nju.edu.cn>
2026-07-17 21:03:55 +0800

    finish lab00 && hw01

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i*i + j*j + k*k - max(i, j, k)**2
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,6 +66,9 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    for i in range(n // 2, 0, -1):
+        if n % i == 0:
+            return i
 
 
 def hailstone(n):
@@ -88,4 +91,15 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    length = 1
+    print(n)
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+        print(n)
+        length += 1
+    return length
+
 
